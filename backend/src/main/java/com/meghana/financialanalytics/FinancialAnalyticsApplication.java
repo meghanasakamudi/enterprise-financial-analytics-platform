4ea685c43387be850ed8bf192d386db01ebe2662
meghanasakamudi <sakamudimeghanams@gmail.com>
2026-08-23 15:36:28 -0500

    Add Spring Boot application entry point

---
+++ backend/src/main/java/com/meghana/financialanalytics/FinancialAnalyticsApplication.java
@@ -0,0 +1,11 @@
+package com.meghana.financialanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialAnalyticsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialAnalyticsApplication.class, args);
+    }
+}
